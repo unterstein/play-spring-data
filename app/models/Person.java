ea134ec097c531fa0a09a295091b23b1e5126868
Johannes Unterstein <unterstein@me.com>
2015-12-18 22:26:26 +0100

    removed doc

--- app/models/Person.java
+++ app/models/Person.java
@@ -3,12 +3,6 @@ package models;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.annotation.Id;
 
-/**
- * This declares a model object for persistence usage. Model objects are generally anaemic structures that represent
- * the database entity. Behaviour associated with instances of a model class are also captured, but behaviours
- * associated with collections of these model objects belong to the PersonRepository e.g. findOne, findAll etc.
- * Play Java will synthesise getter and setter methods for us and therefore keep JPA happy (JPA expects them).
- */
 @Document(indexName = "World")
 public class Person {
     @Id
